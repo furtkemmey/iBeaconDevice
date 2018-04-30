a5721b4466f2e5a245aabaea9ea452f4d065c312
desktop <desktop@desktop.com>
2018-04-30 15:50:59 +0800

    kjpp

--- iBeaconDevice/ViewController.swift
+++ iBeaconDevice/ViewController.swift
@@ -1,25 +1,47 @@
-//
-//  ViewController.swift
-//  iBeaconDevice
-//
-//  Created by perkinsung on 2016/12/14.
-//  Copyright © 2016年 perkinsung. All rights reserved.
-//
-
 import UIKit
+import CoreBluetooth
+import CoreLocation
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController,CBPeripheralManagerDelegate
+{
+    var peripheralManager:CBPeripheralManager!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //初始化藍芽裝置管理員，並指定在Global Queue呼叫代理事件
+        let globalQueue = DispatchQueue.global(qos: .default)
+        //-->觸發藍芽狀態改變時的代理事件
+        peripheralManager = CBPeripheralManager(delegate: self, queue: globalQueue)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //MARK: CBPeripheralManagerDelegate
+    //藍芽狀態改變時  
+    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager)
+    {
+        switch peripheral.state
+        {
+            case .poweredOn:
+                print("藍芽啟動中...")
+            case .poweredOff:
+                print("藍芽未開啟！")
+                return      //離開函式
+            default:
+                return      //離開函式
+        }
+        print("準備啟動iBeacon")
+        
+//        peripheral.delegate = self  //<--可能是多餘的程式
+        //準備iBeacon的UUID
+        let uuid = UUID(uuidString: "70DCA321-1BF2-4BE2-B384-E4595E162EEB")
+        //定義要模擬的iBeacon(major為大分類，minor為小分類，UUID+major+minor為唯一的iBeacon識別裝置)
+        let region = CLBeaconRegion(proximityUUID: uuid!, major: 20000, minor: 499, identifier: "")
+        //取得iBeacon裝置要廣播的字典物件
+        var dic = region.peripheralData(withMeasuredPower: nil) as Dictionary
+        dic.updateValue("這是模擬的iBeacon裝置499" as AnyObject, forKey: CBAdvertisementDataLocalNameKey as NSObject)
+        //開始廣播訊號
+        peripheral.startAdvertising(dic as! [String:AnyObject])
     }
-
 
 }
 
